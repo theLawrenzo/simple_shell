a8dde5c7b6779d626740f2b73d4795dabe8cb340
theLawrenzo <thelawrenzo1@gmail.com>
2023-12-15 19:17:23 -0800

    New updates

---
+++ _read.c
@@ -0,0 +1,55 @@
+#include "shell.h"
+
+/**
+ * _read - Function that reads from a stream
+ * @str: Buffer to read to
+ * @n: Size of the buffer
+ * @stream: Stream to read from
+ *
+ * Return: len
+ */
+ssize_t _read(char **str, size_t *n, FILE *stream)
+{
+	char *currentpos, *newbuf;
+	size_t maxlen, len;
+	int c;
+
+	if (str == NULL && (n == 0 || n))
+	{
+		n = maxlen = 10;
+		len = 0;
+		str = malloc(n);
+		if (str == NULL)
+			return (-1);
+		currentpos = str;
+		while (1)
+		{
+			if ((c = fgetc(stream)) == '\n')
+				break;
+			if (++len >= maxlen)
+			{
+				newbuff = realloc(st, maxlen += n);
+				if (newbuff == NULL)
+				{
+					free(str);
+					return (-1);
+				}
+				currentpos += (currentpos - str);
+				str = newbuff;
+			}
+			*currentpos++ = (char)c;
+			currentpos = '\0';
+		}
+	}
+	else
+	{
+		while (n--)
+		{
+			if ((c = fgetc(stream)) == '\n')
+				break;
+			str[len++] = (char)c;
+		}
+		str[len] = '\0';
+	}
+	return (len - 1);
+}
---
+++ split_str.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * split_str - Function that splits a string
+ * @s: String to split
+ *
+ * Return: tkns
+ */
+char **split_str(char *s, char *sep)
+{
+	char **tkns;
+}
