6c70542031141a9e2d1383c7d39039cf8b465f37
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-02-28 11:25:35 -0800

    An incomplete main.h file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MANI_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wai.h>
+
+#endif
