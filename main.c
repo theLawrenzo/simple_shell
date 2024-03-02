5461bf12ac67af5a1dacbbea6106369eb42fab21
theLawrenzo1 <thelawrenzo1@gmail.com>
2024-03-01 20:45:14 -0800

    main.c  file, run interpreter from here

--- main.c
+++ main.c
@@ -1,14 +1,56 @@
-#include "main.h"
+#include "shell.c"
 
 /**
- * main - call the shell function
+ * main - A simple UNIX command line interpreter
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	if (shell() == -1)
-		exit(EXIT_FAILURE);
+	char *env_var, **args, *path;
+	char *str = NULL;
+	char *exit_cmd = "exit";
+	size_t n = 0;
+	int i;
+
+	while (1)
+	{
+		if (prompt(str, n) == 1)
+		{
+			perror("Error:");
+			exit(EXIT_FAILURE);
+		}
+		env_var = _getenv("PATH");
+		if (env_var == NULL)
+		{
+			perror("Error:");
+			exit(EXIT_FAILURE);
+		}
+		args = split(env_var, " ");
+		if (args == NULL)
+		{
+			perror("Error:");
+			exit(EXIT_FAILURE);
+		}
+		if (args[0] == exit_cmd)
+			break;
+		i = 0;
+		while (args[i] != NULL)
+		{
+			path = find_cmd(args[i]);
+			if (path == NULL)
+			{
+				perror("Error:");
+				exit(EXIT_FAILURE);
+			}
+			if (execute(args) == 1)
+			{
+				perror("Error:");
+				exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+	}
 
 	return (0);
 }
